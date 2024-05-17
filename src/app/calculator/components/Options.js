0050ffcf2aa107fc1cf8e40d3a23f55e31a182f1
xtsebas <thiagohugo2014@gmail.com>
2024-05-17 13:47:10 -0600

    functions implements

--- src/app/calculator/components/Options.js
+++ src/app/calculator/components/Options.js
@@ -1,15 +1,66 @@
+import { useState } from 'react';
+
 import './Options.css';
+import { sum, sub, mul, div, toggleSign, isEqual } from '../operations.js';
 
 const Options = props => {
+    const [Option, setOption] = useState('');
+    const [first, setFirst] = useState(0);
+    const [second, setSecond] = useState(0);
+
+    const handleOptionClick = (option) => {
+        if (Option === '') {
+            setOption(option);
+            console.log(option);
+        }
+        
+        if (first === 0) {
+            setFirst(props.output);
+            props.setOutput('');
+        } else if (second === 0){
+            setSecond(props.output);
+            props.setOutput('');
+        }
+
+        if (first !== 0 && second !== 0 && Option !== '') {
+            
+            if (Option === '+/-') {
+                props.setOutput(toggleSign(props.output));
+            }
+            if (Option === '==') {
+                props.setOutput(isEqual(first, second));
+            }
+            if (option === '=') {
+                switch(option){
+                    case '+':
+                        props.setOutput(sum(first, second));
+                        break;
+                    case '-':
+                        props.setOutput(sub(first, second));
+                        break;
+                    case '*':
+                        props.setOutput(mul(first, second));
+                        break;
+                    case '/':
+                        props.setOutput(div(first, second));
+                        break;
+                }
+            }
+        }
+
+
+
+    };
+
     return (
         <section className="options">
-            <option className="options__option">+</option>
-            <option className="options__option">-</option>
-            <option className="options__option">x</option>
-            <option className="options__option">/</option>
-            <option className="options__option">+/-</option>
-            <option className="options__option"> == </option>
-            <option className="options__option"> = </option>
+            <option className="options__option" onClick={() => handleOptionClick('+')}>+</option>
+            <option className="options__option" onClick={() => handleOptionClick('-')}>-</option>
+            <option className="options__option" onClick={() => handleOptionClick('*')}>x</option>
+            <option className="options__option" onClick={() => handleOptionClick('/')}>/</option>
+            <option className="options__option" onClick={() => handleOptionClick('+/-')}>+/-</option>
+            <option className="options__option" onClick={() => handleOptionClick('==')}> == </option>
+            <option className="options__option" onClick={() => handleOptionClick('=')}> = </option>
         </section>
     );
 };
--- src/app/calculator/operations.js
+++ src/app/calculator/operations.js
@@ -1,3 +1,29 @@
-function sum(){
-    return 1 + 1;
+function sum(first, second){
+    return int(first) + int(second);
 };
+
+function sub(first, second){
+    return int(first) - int(second);
+};  
+
+function mul(first, second){    
+    return int(first) * int(second);
+};
+
+function div(first, second){    
+    return int(first) / int(second);
+};
+
+function int(value){    
+    return parseInt(value);
+};
+
+function toggleSign(number) {
+    return -number;
+};
+
+function isEqual(first, second) {
+    return first === second;
+};
+
+export { sum, sub, mul, div, toggleSign, isEqual };
